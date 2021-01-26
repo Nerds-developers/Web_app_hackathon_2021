4a9bc393bb2d901c749eb53fa8bbccb4a536be9b
Alex Work Laptop <tarasik201617@gmail.com>
2021-01-26 10:48:46 +0200

    Add periodical task.

--- backend/core/tasks.py
+++ backend/core/tasks.py
@@ -1,4 +1,5 @@
 from celery import Celery, group
+from celery.schedules import crontab
 import os
 
 from backend.providers.metro import parse as metro_parser
@@ -23,3 +24,15 @@ def parse_atb():
 @celery.task
 def parse_eko():
     return eko_parser()
+
+
+@celery.task
+def parse_all():
+    task_group = group(parse_metro.s(), parse_atb.s(), parse_eko.s())
+    group_results = task_group()
+    return group_results()
+
+
+@celery.on_after_configure.connect
+def setup_periodic_task(sender, **kwargs):
+    sender.add_periodic_task(crontab(minute="*/3"), parse_all.s(), name="periodic items getting")
