4ea71e3137bf807a9b601c20a107339470fb0385
Alex Work Laptop <tarasik201617@gmail.com>
2021-01-26 16:09:37 +0200

    Change architecture for giving context to the celery.

--- backend/core/tasks.py
+++ backend/core/tasks.py
@@ -1,38 +1,27 @@
-from celery import Celery, group
 from celery.schedules import crontab
-import os
 
-from backend.providers.metro import parse as metro_parser
-from backend.providers.atb import parse as atb_parser
-from backend.providers.eko import parse as eko_parser
+from backend.processing.processing import process
+from backend.web.models import db
+from backend.web import celery
+from backend.web.celery_utils import init_celery
+from backend.web.wsgi_wrapper import application
 
-celery = Celery(__name__,
-                CELERY_BROKER_URL=os.getenv("CELERY_BROKER_URL"),
-                CELERY_RESULT_BACKEND=os.getenv("CELERY_RESULT_BACKEND"))
-
-
-@celery.task
-def parse_metro():
-    return metro_parser()
-
-
-@celery.task
-def parse_atb():
-    return atb_parser()
-
-
-@celery.task
-def parse_eko():
-    return eko_parser()
+init_celery(application, celery)
 
 
 @celery.task
 def parse_all():
-    task_group = group(parse_metro.s(), parse_atb.s(), parse_eko.s())
-    group_results = task_group()
-    return group_results()
+    results = process()
+    for item in results:
+        db.session.add(item)
+    db.session.commit()
 
 
-@celery.on_after_configure.connect
-def setup_periodic_task(sender, **kwargs):
-    sender.add_periodic_task(crontab(minute="*/3"), parse_all.s(), name="periodic items getting")
+celery.conf.beat_schedule = {
+    "fetch-products-to-db": {
+        "task": "backend.core.tasks.parse_all",
+        "schedule": crontab(minute="*/1")
+    }
+}
+
+celery.conf.timezone = "UTC"
--- backend/web/__init__.py
+++ backend/web/__init__.py
@@ -1,31 +1,6 @@
-from flask import Flask
-from flask_restful import Api
-from flask_migrate import Migrate
 import os
+from celery import Celery
 
-from backend.web.api import GrechkaApi
-from backend.web.models import db, ProductItem
-
-
-def create_app():
-    """Application factory for Flask"""
-    app = Flask(__name__)
-    app.config.from_mapping(
-        SQLALCHEMY_TRACK_MODIFICATIONS=False,
-        SQLALCHEMY_DATABASE_URI=os.getenv("DB_URI"),
-        CELERY_BROKER_URL=os.getenv("CELERY_BROKER_URL"),
-        CELERY_RESULT_BACKEND=os.getenv("CELERY_RESULT_BACKEND")
-    )
-    api_instance = Api(app)
-    api_instance.add_resource(GrechkaApi, "/api/grechka")
-    db.init_app(app)
-    Migrate(app, db)
-
-    app.app_context().push()
-    db.create_all()
-
-    item = ProductItem(title="h", price=1, producer="p", prod_link="http", image_link="http")
-    db.session.add(item)
-    db.session.commit()
-
-    return app
+celery = Celery(__name__,
+                CELERY_BROKER_URL=os.getenv("CELERY_BROKER_URL"),
+                CELERY_RESULT_BACKEND=os.getenv("CELERY_RESULT_BACKEND"))
